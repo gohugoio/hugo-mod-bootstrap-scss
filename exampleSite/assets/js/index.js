7cdd1abb153b7b89f19b1bfb825331b846c3d7af
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-10-21 21:15:16 +0200

    Add Button and Carousel JS examples
    Updates #16

--- exampleSite/assets/js/index.js
+++ exampleSite/assets/js/index.js
@@ -1,22 +1,22 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
-import Toast from "js/bootstrap/src/toast";
-import Popover from "js/bootstrap/src/popover";
+import Toast from 'js/bootstrap/src/toast';
+import Popover from 'js/bootstrap/src/popover';
+import Button from 'js/bootstrap/src/button.js';
+import Carousel from 'js/bootstrap/src/carousel.js';
 
 (function () {
-  let toastElList = [].slice.call(document.querySelectorAll(".toast"));
-  let toastList = toastElList.map(function (toastEl) {
-    return new Toast(toastEl);
-  });
+	let toastElList = [].slice.call(document.querySelectorAll('.toast'));
+	let toastList = toastElList.map(function (toastEl) {
+		return new Toast(toastEl);
+	});
 
-  toastList.forEach(function (toast) {
-    toast.show();
-  });
+	toastList.forEach(function (toast) {
+		toast.show();
+	});
 
-  let popoverTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="popover"]')
-  );
-  popoverTriggerList.map(function (popoverTriggerEl) {
-    return new Popover(popoverTriggerEl);
-  });
+	let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+	popoverTriggerList.map(function (popoverTriggerEl) {
+		return new Popover(popoverTriggerEl);
+	});
 })();
